0fd69218dac0432971e4360b42781b067eadc7ac
Phuong Nguyen <phuongnk@yahoo.com>
2015-11-02 23:15:03 -0500

    Initial add of files to repository

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by Owner on 11/1/2015.
+ */
+//package com.bignerdranch.android.geoquiz;
+
+public class Question {
+    private int mQuestionStringResourceId;
+    private boolean mAnswer;
+
+    public Question(int pTextResourceId, boolean pAnswer) {
+        mQuestionStringResourceId = pTextResourceId;
+        mAnswer = pAnswer;
+    }
+
+    public int getQuestionStringResourceId() {
+        return mQuestionStringResourceId;
+    }
+
+    public void setQuestionStringResourceId(int pQuestionStringResourceId) {
+        mQuestionStringResourceId = pQuestionStringResourceId;
+    }
+
+    public boolean isAnswer() {
+        return mAnswer;
+    }
+
+    public void setAnswer(boolean pAnswer) {
+        mAnswer = pAnswer;
+    }
+}
---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -0,0 +1,105 @@
+package com.bignerdranch.android.geoquiz;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Toast mButtonToast;
+    private TextView mQuestionTextView;
+    private Button mNextButton;
+    private Question[] mQuestionBank = new Question[]{
+                new Question(R.string.question_oceans, true),
+                new Question(R.string.question_mideast, false),
+                new Question(R.string.question_africa, false),
+                new Question(R.string.question_americas, true),
+                new Question(R.string.question_asia, true),
+                new Question(R.string.question_ann, true)
+            };
+    private int mCurrentIndex = 0;
+
+    private void updateQuestion() {
+        int questionTextResourceId = mQuestionBank[mCurrentIndex].getQuestionStringResourceId();
+        mQuestionTextView.setText(questionTextResourceId);
+    }
+
+    private void checkAnswer(boolean mButtonPressed) {
+        int msgResourceId = 0;
+        if (mQuestionBank[mCurrentIndex].isAnswer() == mButtonPressed) {
+            msgResourceId = R.string.correct_toast_msg;
+        } else {
+            msgResourceId = R.string.incorrect_toast_msg;
+        }
+
+        Toast.makeText(this, msgResourceId, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        mNextButton = (Button) findViewById(R.id.next_button);
+
+        updateQuestion();
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(true);
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(false);
+            }
+        });
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
